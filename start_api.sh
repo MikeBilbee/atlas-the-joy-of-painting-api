ac94c2450ce692bb152c7cfd41c20e548b82cea5
MikeBilbee <cmbilbee@gmail.com>
2024-07-28 13:38:07 -0500

    auto start script

---
+++ start_api.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-pip
+npm install
+pip install -r requirements.txt
+python3 modify.py 
+python3 setup_db.py
+uvicorn app:app --reload
